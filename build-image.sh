e51208dfc991cf4b86e58b1cb9eae1cd1b8c5ca5
Simon Lelievre <simon.lelievre@corp.ovh.com>
2016-07-25 18:09:47 +0200

    build image script

---
+++ build-image.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+grep -v " overthebox " overthebox-openwrt/feeds.conf.default > overthebox-openwrt/feeds.conf
+echo "src-link overthebox $(readlink -f overthebox-feeds)" >> overthebox-openwrt/feeds.conf
+
+cp config overthebox-openwrt/.config
+
+cd overthebox-openwrt
+./scripts/feeds update -a
+./scripts/feeds install -a -p overthebox
+./scripts/feeds install -p overthebox -f netifd
+./scripts/feeds install -p overthebox -f dnsmasq
+./scripts/feeds install -a
+
+make defconfig
+
+make -j$(nproc)
+
+./scripts/diffconfig.sh  > bin/x86-glibc/config
+
--- build.sh
+++
@@ -1,5 +0,0 @@
-
-
-grep -v " overthebox " overthebox-openwrt/feeds.conf.default > overthebox-openwrt/feeds.conf
-echo "src-link overthebox $(readlink -f overthebox-feeds)" >> overthebox-openwrt/feeds.conf
-
