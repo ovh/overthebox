777ae3d0a8ab07afd760297dad4778993dcc0996
Adrien Gallouët <adrien@gallouet.fr>
2016-09-22 09:32:24 +0000

    Use the same branch for overthebox-openwrt

--- build-image.sh
+++ build-image.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+TAG=$(git describe --tags --match='v[0-9].*')
+
 [ -d overthebox-openwrt ] || \
-    git clone --depth=1 https://github.com/ovh/overthebox-openwrt --branch overthebox
+    git clone --depth=1 https://github.com/ovh/overthebox-openwrt --branch ${BRANCH}
 
 rsync -avh otb/ overthebox-openwrt/
 
@@ -17,31 +20,10 @@ make dirclean
 cp ../config .config
 make defconfig
 
-# compile tools and toolchain
-make tools/install -j$(nproc)
-make toolchain/install -j$(nproc)
-make package/toolchain/compile -j$(nproc)
-
-# compile first package to avoid compilation error :/
-make package/toolchain/install -j$(nproc)
-make package/lua/host/compile -j$(nproc)
-make package/luci-base/host/compile -j$(nproc)
-make package/intltool/host/compile -j$(nproc)
-make package/gettext-full/host/compile -j$(nproc)
-make package/grub2-efi/host/compile -j$(nproc)
-make package/grub2/host/compile -j$(nproc)
-make package/ncurses/install -j$(nproc)
-make package/ncurses/host/install -j$(nproc)
-
-# full compile
 make -j$(nproc)
 
 ./scripts/diffconfig.sh  > bin/x86-glibc/config
 cp -a bin/x86-glibc/packages/overthebox bin/x86-glibc/packages/ovh
 cp ./staging_dir/target-x86_64_glibc-2.21/root-x86/lib/upgrade/platform.sh bin/x86-glibc/
 
-if [ -n "$RSYNC" ];
-then
-    TAG=$(git describe --tags --match='v[0-9].*')
-    rsync -a bin/x86-glibc/ $RSYNC/$TAG
-fi
+[ -n "${RSYNC}" ] && rsync -a bin/x86-glibc/ ${RSYNC}/${TAG}
