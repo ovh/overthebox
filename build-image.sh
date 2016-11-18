e17a2fb027bf32a739da5e0cf5c95a2e691a563a
Sebastien DUPONCHEEL <sebastien.duponcheel@corp.ovh.com>
2016-11-18 16:01:33 +0100

    Updated build image script

--- build-image.sh
+++ build-image.sh
@@ -1,28 +1,38 @@
 #!/usr/bin/env bash
 set -e
 
-export OPENWRT_VERSION=`git ls-remote --tags https://github.com/openwrt/openwrt | awk -F/ '{ print $3 }' | sort -r | head -n1 2>/dev/null`
-export OTB_TAG=`git ls-remote --tags https://github.com/ovh/overthebox-feeds | awk -F/ '{ print $3 }' | sort -r | head -n1 2>/dev/null`
-export OTB_VERSION=${OTB_TAG#v}
+export OPENWRT_VERSION="3f98448d670ab2e908c8d6002d8c6f8ff5d1d9bd"
+# export OPENWRT_VERSION=`git ls-remote --tags https://github.com/openwrt/openwrt | awk -F/ '{ print $3 }' | sort -r | head -n1 2>/dev/null`
+export OTB_TAG=`git ls-remote --tags https://github.com/ovh/overthebox-feeds | rev | sort -r -V -t. | rev | grep -v "\^{}$" | head -n1 2>/dev/null`
+export OTB_VERSION=`echo "$OTB_TAG" | awk -F/ '{ print $3 }'`
+export OTB_COMMIT=`echo "$OTB_TAG" | cut -f1`
 
-echo "Openwrt version: $OPENWRT_VERSION"
-echo "OverTheBox version: $OTB_VERSION"
+echo "--- Openwrt version: $OPENWRT_VERSION	---"
+echo "--- OverTheBox version: $OTB_VERSION	---"
+echo "--- OverTheBox commit: $OTB_COMMIT	---"
 
-[ -d overthebox-openwrt ] || \
-	git clone --depth=1 https://github.com/openwrt/openwrt.git --tag ${OPENWRT_TAG} overthebox-openwrt
+[ -d overthebox-openwrt ] || {
+	git clone https://github.com/openwrt/openwrt.git overthebox-openwrt
+	cd overthebox-openwrt
+	git checkout ${OPENWRT_VERSION}
+	cd ..
+#	git clone --depth=1 https://github.com/openwrt/openwrt.git --tag ${OPENWRT_TAG} overthebox-openwrt
+}
 
 rsync -avh otb/ overthebox-openwrt/
-
 cd overthebox-openwrt
+sed -i "/^src-git overthebox/ s/$/^$OTB_COMMIT/" feeds.conf
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a -f -p overthebox
 ./scripts/feeds install -a
+rsync -avh ../otb/feeds feeds
 
 make dirclean
 
 cp ../config .config
 make defconfig
+# make kernel_menuconfig CONFIG_TARGET=subtarget
 
 make -j$(nproc)
 
