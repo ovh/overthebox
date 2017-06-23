10ca58b26e9cda5d082495a576115c09604da081
Adrien Gallouët <angt@users.noreply.github.com>
2017-06-23 09:06:19 +0200

    Update build process (#74)
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- build.sh
+++ build.sh
@@ -2,34 +2,41 @@
 
 set -e
 
-OTB_SOURCE=https://github.com/ovh/overthebox-lede
-OTB_NUMBER=17.06.18
-OTB_VERSION=$(git rev-parse --short HEAD)
+OTB_NUMBER=$(git describe --tag --always)
 
-[ -d source ] || \
-	git clone --depth 1 "$OTB_SOURCE" --branch "otb-$OTB_NUMBER" source
-
-rsync -avh custom/ source/
-
-cd source
-
-cp .config .config.keep
-scripts/feeds update -a
-scripts/feeds install -a -d y -f -p overthebox
-cp .config.keep .config
-
-OTB_FEEDS_VERSION=$(git -C feeds/overthebox rev-parse --short HEAD)
+OTB_SRC=${OTB_SRC:-17.06.18}
 OTB_REPO=${OTB_REPO:-http://$(curl -sS ipaddr.ovh):8000}
 OTB_DIST=${OTB_DIST:-otb}
 
-if [ -n "$1" ] && [ -d "feeds/overthebox/$1" ]; then
-    OTB_DIST=$1
-    shift 1
+git clone https://github.com/ovh/overthebox-lede source || true
+git -C source fetch --all
+git -C source checkout "origin/otb-$OTB_SRC" -B "otb-$OTB_SRC"
+
+feed=${OTB_FEED:-feed}
+
+if [ -z "$OTB_FEED" ]; then
+	OTB_FEED_SRC=${OTB_FEED_SRC:-master}
+	git clone https://github.com/ovh/overthebox-feeds "$feed" || true
+	git -C "$feed" fetch --all
+	git -C "$feed" checkout "origin/$OTB_FEED_SRC" -B "$OTB_FEED_SRC"
 fi
 
-echo "$OTB_VERSION-$OTB_FEEDS_VERSION" > version
+echo "$OTB_SRC-$(git -C "$feed" describe --tag --always)" > source/version
 
-cat >> .config <<EOF
+if [ -n "$1" ] && [ -d "$feed/$1" ]; then
+	OTB_DIST=$1
+	shift 1
+fi
+
+rsync -avh custom/ source/
+
+cat > source/feeds.conf <<EOF
+src-git packages https://git.lede-project.org/feed/packages.git;lede-17.01
+src-git luci https://github.com/openwrt/luci.git;for-15.05
+src-link overthebox $(readlink -f "$feed")
+EOF
+
+cat >> source/.config <<EOF
 CONFIG_IMAGEOPT=y
 CONFIG_VERSIONOPT=y
 CONFIG_VERSION_DIST="$OTB_DIST"
@@ -38,6 +45,14 @@ CONFIG_VERSION_NUMBER="$OTB_NUMBER"
 CONFIG_PACKAGE_$OTB_DIST=y
 EOF
 
+cd source
+
+echo "Building $(cat version)"
+
+cp .config .config.keep
+scripts/feeds update -a
+scripts/feeds install -a -d y -f -p overthebox
+cp .config.keep .config
+
 make defconfig
-make clean
 make "$@"
